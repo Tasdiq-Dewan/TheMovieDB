9f687590fc4b80645a54ec4a14d582258166ebff
Tasdiq-Dewan <tasdiqd@gmail.com>
2023-04-16 00:43:30 +0100

    created preview templates

--- app/src/main/java/com/tasdiqdewan/themoviedb/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/tasdiqdewan/themoviedb/ui/screens/home/HomeScreen.kt
@@ -1,6 +1,10 @@
 package com.tasdiqdewan.themoviedb.ui.screens.home
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.tasdiqdewan.themoviedb.ui.theme.TheMovieDBTheme
@@ -9,12 +13,24 @@ import com.tasdiqdewan.themoviedb.ui.theme.TheMovieDBTheme
 fun HomeScreen(
     state: HomeScreenState
 ) {
-    
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        
+    }
 }
 
-@Preview
+@Preview(showSystemUi = true, showBackground = true, device = "id:pixel_5")
 @Composable
 fun HomeScreenPreview() {
+    TheMovieDBTheme {
+        HomeScreen(state = HomeScreenState())
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true, device = "id:pixel_5", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun HomeScreenPreviewDark() {
     TheMovieDBTheme {
         HomeScreen(state = HomeScreenState())
     }
