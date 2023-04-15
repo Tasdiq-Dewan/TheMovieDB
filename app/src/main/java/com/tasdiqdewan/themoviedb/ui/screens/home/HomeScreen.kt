52da7d183fa493f544f4cdbab5b71aa798ffaa9c
Tasdiq-Dewan <tasdiqd@gmail.com>
2023-04-16 00:38:19 +0100

    empty HomeScreen composable

--- app/src/main/java/com/tasdiqdewan/themoviedb/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/tasdiqdewan/themoviedb/ui/screens/home/HomeScreen.kt
@@ -1,2 +1,21 @@
 package com.tasdiqdewan.themoviedb.ui.screens.home
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.tasdiqdewan.themoviedb.ui.theme.TheMovieDBTheme
+
+@Composable
+fun HomeScreen(
+    state: HomeScreenState
+) {
+    
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    TheMovieDBTheme {
+        HomeScreen(state = HomeScreenState())
+    }
+}
