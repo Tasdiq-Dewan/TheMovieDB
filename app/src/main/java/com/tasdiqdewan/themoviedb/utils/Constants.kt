a6ec90577f5a63a53cec4905bfa03ec45dec7696
Tasdiq-Dewan <tasdiqd@gmail.com>
2023-04-15 17:52:55 +0100

    Created Constants

---
+++ app/src/main/java/com/tasdiqdewan/themoviedb/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.tasdiqdewan.themoviedb.utils
+
+object Constants {
+    const val API_KEY = "50c2d1450717884bac15ef6b47db308c"
+    const val BEARER_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MGMyZDE0NTA3MTc4ODRiYWMxNWVmNmI0N2RiMzA4YyIsInN1YiI6IjYzZmQzZGFlN2E0ZWU3MDBmMGY4Y2VmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hyJ7c2G4yRo44UhvrdL3k7eFq10ahxgYIhnTKmq7lLs"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/"
+}
