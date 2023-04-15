2b3121429f9cb62add13bb79713d829d512c8bd1
Tasdiq-Dewan <tasdiqd@gmail.com>
2023-04-15 01:17:51 +0100

    disabled dynamic colours

--- app/src/main/java/com/tasdiqdewan/themoviedb/MainActivity.kt
+++ app/src/main/java/com/tasdiqdewan/themoviedb/MainActivity.kt
@@ -3,13 +3,21 @@ package com.tasdiqdewan.themoviedb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tasdiqdewan.themoviedb.ui.theme.TheMovieDBTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +39,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    Card(
         modifier = modifier
-    )
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp)
+        ) {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+            Button(
+                onClick = { /*TODO*/ },
+            ) {
+                Text("Button")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/tasdiqdewan/themoviedb/ui/theme/Theme.kt
+++ app/src/main/java/com/tasdiqdewan/themoviedb/ui/theme/Theme.kt
@@ -81,7 +81,7 @@ private val LightColorScheme = lightColorScheme(
 fun TheMovieDBTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
