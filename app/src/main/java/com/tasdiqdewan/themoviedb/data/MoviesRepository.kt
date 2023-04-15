00069b317634a9390e4d500cc3dd6431aa1b31d8
Tasdiq-Dewan <tasdiqd@gmail.com>
2023-04-16 00:29:55 +0100

    fixed repository scope to inject into viewmodel

--- app/src/main/java/com/tasdiqdewan/themoviedb/data/MoviesRepository.kt
+++ app/src/main/java/com/tasdiqdewan/themoviedb/data/MoviesRepository.kt
@@ -1,9 +1,10 @@
 package com.tasdiqdewan.themoviedb.data
 
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
-@ActivityScoped
+@ActivityRetainedScoped
 class MoviesRepository @Inject constructor(
     private val apiServices: ApiServices
 ) {
