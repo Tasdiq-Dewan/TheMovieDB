6aecc65c0e491903fa2491b3c89a25b4797fc54d
Tasdiq-Dewan <tasdiqd@gmail.com>
2023-04-17 19:22:14 +0100

    added loading and broken image resources

--- compose_library/src/main/java/com/tasdiqdewan/compose_library/composables/MovieResult.kt
+++ compose_library/src/main/java/com/tasdiqdewan/compose_library/composables/MovieResult.kt
@@ -1,6 +1,12 @@
 package com.tasdiqdewan.compose_library.composables
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun MovieResult(
@@ -9,7 +15,30 @@ fun MovieResult(
     releaseDate: String,
     posterPath: String,
     backdropPath: String,
-    voteAverage: Double
+    voteAverage: Double,
+    modifier: Modifier = Modifier
 ) {
+    Card(
+        modifier = modifier
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = title)
+            Text(text = releaseDate)
+        }
+    }
+}
 
+@Preview
+@Composable
+fun MovieResultPreview() {
+    MovieResult(
+        id = 1,
+        title = "The Super Mario Bros. Movie",
+        releaseDate = "2023-04-05",
+        posterPath = "",
+        backdropPath = "",
+        voteAverage = 5.0
+    )
 }
