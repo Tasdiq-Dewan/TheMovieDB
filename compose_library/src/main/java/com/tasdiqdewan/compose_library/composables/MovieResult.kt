5475e42d49c89f3e42b8ba22ecbe152239d922e0
Tasdiq-Dewan <tasdiqd@gmail.com>
2023-04-17 19:04:03 +0100

    definite empty MovieResult composable

---
+++ compose_library/src/main/java/com/tasdiqdewan/compose_library/composables/MovieResult.kt
@@ -0,0 +1,15 @@
+package com.tasdiqdewan.compose_library.composables
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MovieResult(
+    id: Int,
+    title: String,
+    releaseDate: String,
+    posterPath: String,
+    backdropPath: String,
+    voteAverage: Double
+) {
+
+}
