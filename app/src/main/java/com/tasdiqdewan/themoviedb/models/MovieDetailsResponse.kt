8dcc5116cfab87c1132b6c84f28caa1a0a29a798
Tasdiq-Dewan <tasdiqd@gmail.com>
2023-04-29 23:39:49 +0100

    corrected response models

--- app/src/main/java/com/tasdiqdewan/themoviedb/models/MovieDetailsResponse.kt
+++ app/src/main/java/com/tasdiqdewan/themoviedb/models/MovieDetailsResponse.kt
@@ -3,49 +3,49 @@ package com.tasdiqdewan.themoviedb.models
 import com.squareup.moshi.Json
 
 data class MovieDetailsResponse(
-    val adult: Boolean?,
+    val adult: Boolean,
     @Json(name = "backdrop_path") val backdropPath: String?,
     @Json(name = "belongs_to_collection") val belongsToCollection: MovieCollection?,
-    val budget: Int?,
-    val genres: List<Genre>?,
+    val budget: Int,
+    val genres: List<Genre>,
     val homepage: String?,
-    val id: Int?,
+    val id: Int,
     @Json(name = "imdb_id") val imdbId: String?,
-    @Json(name = "original_language") val originalLanguage: String?,
-    @Json(name = "original_title") val originalTitle: String?,
+    @Json(name = "original_language") val originalLanguage: String,
+    @Json(name = "original_title") val originalTitle: String,
     val overview: String?,
-    val popularity: Double?,
+    val popularity: Double,
     @Json(name = "poster_path") val posterPath: String?,
-    @Json(name = "production_companies") val productionCompanies: List<ProductionCompany>?,
-    @Json(name = "production_countries") val productionCountries: List<Country>?,
-    @Json(name = "release_date") val releaseDate: String?,
-    val revenue: Long?,
+    @Json(name = "production_companies") val productionCompanies: List<ProductionCompany>,
+    @Json(name = "production_countries") val productionCountries: List<Country>,
+    @Json(name = "release_date") val releaseDate: String,
+    val revenue: Long,
     val runtime: Int?,
-    @Json(name = "spoken_languages") val spokenLanguages: List<Language>?,
-    val status: String?,
-    val tagline: String?,
-    val title: String?,
-    val video: Boolean?,
-    @Json(name = "vote_average") val voteAverage: Double?,
-    @Json(name = "vote_count") val voteCount: Int?
+    @Json(name = "spoken_languages") val spokenLanguages: List<Language>,
+    val status: String,
+    val tagline: String,
+    val title: String,
+    val video: Boolean,
+    @Json(name = "vote_average") val voteAverage: Double,
+    @Json(name = "vote_count") val voteCount: Int
 ) {
     data class MovieCollection(
         val id: Int,
         val name: String,
-        @Json(name = "backdrop_path") val backdropPath: String,
-        @Json(name = "poster_path") val posterPath: String,
+        @Json(name = "backdrop_path") val backdropPath: String?,
+        @Json(name = "poster_path") val posterPath: String?,
     )
 
     data class Genre(
-        val id: Int?,
-        val name: String?
+        val id: Int,
+        val name: String
     )
 
     data class ProductionCompany(
-        val id: Int?,
+        val id: Int,
         @Json(name = "logo_path") val logoPath: String?,
-        val name: String?,
-        @Json(name = "origin_country") val originCountry: String?
+        val name: String,
+        @Json(name = "origin_country") val originCountry: String
     )
 
     data class Country(
