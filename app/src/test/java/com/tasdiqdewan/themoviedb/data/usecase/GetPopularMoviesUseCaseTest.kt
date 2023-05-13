8ed4ce7ab41b2cded3768e4fe7259b4308ae232e
Tasdiq-Dewan <tasdiqd@gmail.com>
2023-05-13 12:09:43 +0100

    GetPopularMoviesUseCaseTest

--- app/src/test/java/com/tasdiqdewan/themoviedb/data/usecase/GetPopularMoviesUseCaseTest.kt
+++ app/src/test/java/com/tasdiqdewan/themoviedb/data/usecase/GetPopularMoviesUseCaseTest.kt
@@ -3,8 +3,10 @@ package com.tasdiqdewan.themoviedb.data.usecase
 import android.util.Log
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.tasdiqdewan.themoviedb.data.repository.MoviesRepository
+import com.tasdiqdewan.utils.dto.MovieDetailsResponseDto
 import com.tasdiqdewan.utils.fake.FakeDataSource
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkStatic
@@ -13,13 +15,18 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
+import retrofit2.HttpException
 import retrofit2.Response
+import java.io.IOException
+import kotlin.test.assertFailsWith
 
 @ExperimentalCoroutinesApi
 class GetPopularMoviesUseCaseTest {
@@ -54,5 +61,34 @@ class GetPopularMoviesUseCaseTest {
 
         //then
         assertEquals(FakeDataSource.FAKE_POPULAR_MOVIES_RESPONSE.results, result)
+        coVerify { repository.getPopularMoviesList(any()) }
+    }
+
+    @Test
+    fun getPopularMoviesUseCase_shouldThrowIOException() = runTest {
+        //given
+        coEvery { repository.getPopularMoviesList(any()) } throws IOException()
+
+        //then
+        assertFailsWith<IOException> {
+            val result = getPopularMoviesUseCase.execute()
+        }
+    }
+
+    @Test
+    fun getPopularMoviesUseCase_shouldThrowHttpException() = runTest {
+        //given
+        val response: Response<MovieDetailsResponseDto> = Response.error(
+            400,
+            "{\"key\":[\"somestuff\"]}"
+                .toResponseBody("application/json".toMediaTypeOrNull())
+        )
+        val exception = HttpException(response)
+        coEvery { repository.getPopularMoviesList(any()) } throws exception
+
+        //then
+        assertFailsWith<HttpException> {
+            val result = getPopularMoviesUseCase.execute()
+        }
     }
 }
